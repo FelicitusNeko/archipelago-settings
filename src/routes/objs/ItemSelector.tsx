a9f894ec4008066496dce21ffb8947bb1e18df9f
Felix <felicitusneko@vivaldi.net>
2021-11-23 19:35:51 -0400

    itemsel: added rest of function

--- src/routes/objs/ItemSelector.tsx
+++ src/routes/objs/ItemSelector.tsx
@@ -1,3 +1,4 @@
+import Slider from "rc-slider/lib/Slider";
 import React, { useState, useEffect } from "react";
 import {
   DragDropContext,
@@ -17,23 +18,60 @@ import "./ItemSelector.css";
 
 interface ItemNodeProps {
   id: string;
+  category: string;
   index: number;
   children: string;
+  hint: boolean;
   max?: number;
   qty?: number;
+  onChange: CommonItemSettingChangeEvent;
 }
-const ItemNode: React.FC<ItemNodeProps> = ({ id, index, children }) => {
+const ItemNode: React.FC<ItemNodeProps> = ({
+  id,
+  category,
+  index,
+  children,
+  hint,
+  max,
+  qty,
+  onChange,
+}) => {
+  const [draggable, setDraggable] = useState(true);
+
+  const onHintChange: React.ChangeEventHandler<HTMLInputElement> = ({currentTarget}) =>
+    onChange(id, category, {startingHint: currentTarget.checked})
+
+  const onQtyChange = (newVal: number) =>
+    onChange(id, category, {qty: newVal})
+
   return (
-    <Draggable draggableId={id} index={index}>
+    <Draggable draggableId={id} index={index} isDragDisabled={!draggable}>
       {(provided) => {
         return (
           <div
             className="itemnode"
+            style={!draggable ? { backgroundColor: "orange" } : undefined}
             {...provided.draggableProps}
             {...provided.dragHandleProps}
             ref={provided.innerRef}
           >
+            {max === undefined || max !== qty ? (
+              <input type="checkbox" checked={hint} onChange={onHintChange} />
+            ) : null}
+            {qty && max && max > 2 ? `${qty}× ` : null}
             {children}
+            {qty && max && max > 2 ? (
+              <>
+                <br />
+                <div
+                  onMouseEnter={() => setDraggable(false)}
+                  onMouseLeave={() => setDraggable(true)}
+                  style={{ width: "90%", margin: "auto" }}
+                >
+                  <Slider min={1} max={max} value={qty} onChange={onQtyChange} />
+                </div>
+              </>
+            ) : null}
           </div>
         );
       }}
@@ -43,15 +81,21 @@ const ItemNode: React.FC<ItemNodeProps> = ({ id, index, children }) => {
 
 interface ItemDropboxProps {
   id: string;
+  category: string;
   title: string;
   items?: ArchipelagoItem[];
   itemsAndQtys?: ArchipelagoItemAndQty[];
+  start_hints?: ArchipelagoItem[];
+  onChange: CommonItemSettingChangeEvent;
 }
 const ItemDropbox: React.FC<ItemDropboxProps> = ({
   id,
+  category,
   title,
   items,
   itemsAndQtys,
+  start_hints,
+  onChange,
 }) => {
   if ((!items && !itemsAndQtys) || (items && itemsAndQtys)) return null;
   return (
@@ -67,7 +111,14 @@ const ItemDropbox: React.FC<ItemDropboxProps> = ({
             >
               {items
                 ? items.map((i, x) => (
-                    <ItemNode key={`item-${i.name}`} id={i.name} index={x}>
+                    <ItemNode
+                      key={`item-${i.name}`}
+                      id={i.name}
+                      category={category}
+                      index={x}
+                      hint={start_hints ? start_hints.includes(i) : false}
+                      onChange={onChange}
+                    >
                       {i.readableName ?? i.name}
                     </ItemNode>
                   ))
@@ -75,9 +126,12 @@ const ItemDropbox: React.FC<ItemDropboxProps> = ({
                     <ItemNode
                       key={`item-${item.name}`}
                       id={item.name}
+                      category={category}
                       index={x}
                       max={item.max === undefined ? 1 : item.max}
                       qty={qty}
+                      hint={start_hints ? start_hints.includes(item) : false}
+                      onChange={onChange}
                     >
                       {item.readableName ?? item.name}
                     </ItemNode>
@@ -104,7 +158,8 @@ const ItemSelector: React.FC<ItemSelectorProps> = ({
   onChange,
 }) => {
   const [unassigned, setUnassigned] = useState(items);
-  const { local_items, non_local_items, start_inventory } = commonSettings;
+  const { local_items, non_local_items, start_inventory, start_hints } =
+    commonSettings;
 
   useEffect(() => {
     setUnassigned(
@@ -121,7 +176,7 @@ const ItemSelector: React.FC<ItemSelectorProps> = ({
         )
         .filter((i) =>
           commonSettings.start_inventory
-            ? !commonSettings.start_inventory.map(i => i.item).includes(i)
+            ? !commonSettings.start_inventory.map((i) => i.item).includes(i)
             : true
         )
     );
@@ -158,7 +213,7 @@ const ItemSelector: React.FC<ItemSelectorProps> = ({
         break;
       case "start_inventory":
         if (!start_inventory) return;
-        ({ item: moveItem/*, qty: moveQty*/ } =
+        ({ item: moveItem /*, qty: moveQty*/ } =
           start_inventory[result.source.index]);
         break;
       default:
@@ -175,29 +230,49 @@ const ItemSelector: React.FC<ItemSelectorProps> = ({
   };
 
   return (
-    <div className="itemselector">
-      <DragDropContext onDragEnd={onDragEnd}>
-        <ItemDropbox
-          id="unassigned"
-          title="Unassigned items"
-          items={unassigned}
-        />
-        <ItemDropbox
-          id="local_items"
-          title="Local items"
-          items={local_items ?? []}
-        />
-        <ItemDropbox
-          id="non_local_items"
-          title="Non-Local items"
-          items={non_local_items ?? []}
-        />
-        <ItemDropbox
-          id="start_inventory"
-          title="Starting inventory"
-          itemsAndQtys={start_inventory ?? []}
-        />
-      </DragDropContext>
+    <div className="setting">
+      <b>Item selector</b>
+      <br />
+      Select which items are guaranteed to be in your world (local) or not
+      (non-local), and items you start with. Check the box if you want to get a
+      hint on where that item is from the start.
+      <br />
+      <div className="itemselector">
+        <DragDropContext onDragEnd={onDragEnd}>
+          <ItemDropbox
+            id="unassigned"
+            category={category}
+            title="Unassigned items"
+            items={unassigned}
+            start_hints={start_hints}
+            onChange={onChange}
+          />
+          <ItemDropbox
+            id="local_items"
+            category={category}
+            title="Local items"
+            items={local_items ?? []}
+            start_hints={start_hints}
+            onChange={onChange}
+          />
+          <ItemDropbox
+            id="non_local_items"
+            category={category}
+            title="Non-Local items"
+            items={non_local_items ?? []}
+            start_hints={start_hints}
+            onChange={onChange}
+          />
+          <ItemDropbox
+            id="start_inventory"
+            category={category}
+            title="Starting inventory"
+            itemsAndQtys={start_inventory ?? []}
+            start_hints={start_hints}
+            onChange={onChange}
+          />
+        </DragDropContext>
+      </div>
     </div>
   );
 };
