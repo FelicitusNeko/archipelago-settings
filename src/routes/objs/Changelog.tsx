f78937c3379e9f7f141adc7e5c8fdedfb68399ee
Felix <felicitusneko@vivaldi.net>
2022-06-07 19:29:39 -0300

    formatting cleanup

--- src/routes/objs/Changelog.tsx
+++ src/routes/objs/Changelog.tsx
@@ -2,7 +2,10 @@ import React from "react";
 import ReactMarkdown from "react-markdown";
 
 type ChangelogLine = (string | ChangelogLine)[];
-const changelogData:Record<string, ChangelogLine> = require("../../defs/changelog.json");
+const changelogData: Record<
+  string,
+  ChangelogLine
+> = require("../../defs/changelog.json");
 
 const Changelog: React.FC = () => {
   /** Parsed data to display in the changelog. */
@@ -13,20 +16,23 @@ const Changelog: React.FC = () => {
    * @param level The nesting level of the current list.
    */
   const Reader = (data: ChangelogLine, level = 0) => {
-    const spacing = (() => {let retval = ''; for (let x = 0; x < level; x++) retval += '  '; return retval + "- ";})()
-    for (const line of data) if (Array.isArray(line)) Reader(line, level + 1);
-    else parsedData.push(spacing + line);
-  }
+    const spacing = (() => {
+      let retval = "";
+      for (let x = 0; x < level; x++) retval += "  ";
+      return retval + "- ";
+    })();
+    for (const line of data)
+      if (Array.isArray(line)) Reader(line, level + 1);
+      else parsedData.push(spacing + line);
+  };
   for (const version in changelogData) {
     parsedData.push(`#### ${version}`);
     Reader(changelogData[version]);
-    parsedData.push('---');
+    parsedData.push("---");
   }
   parsedData.pop();
 
-  return (
-    <ReactMarkdown children={parsedData.join('\n')} />
-  );
+  return <ReactMarkdown children={parsedData.join("\n")} />;
 };
 
 export default Changelog;
