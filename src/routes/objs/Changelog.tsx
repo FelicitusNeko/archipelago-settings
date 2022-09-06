9a32f97e149a51d74afb8b1a7db79f6e82142f0a
Felix R <felicitusneko@duck.com>
2022-09-06 00:40:31 -0300

    paginate changelog

--- src/routes/objs/Changelog.tsx
+++ src/routes/objs/Changelog.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactMarkdown from "react-markdown";
 
 type ChangelogLine = (string | ChangelogLine)[];
@@ -8,33 +8,66 @@ const changelogData: Record<
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 > = require("../../defs/changelog.json");
 
-const Changelog: React.FC = () => {
-  /** Parsed data to display in the changelog. */
-  const parsedData: string[] = [];
-  /**
-   * Reads a list of changelog data to be displayed.
-   * @param data The data to read.
-   * @param level The nesting level of the current list.
-   */
-  const Reader = (data: ChangelogLine, level = 0) => {
-    const spacing = (() => {
-      let retval = "";
-      for (let x = 0; x < level; x++) retval += "  ";
-      return retval + "- ";
-    })();
-    for (const line of data)
-      if (Array.isArray(line)) Reader(line, level + 1);
-      else parsedData.push(spacing + line);
-  };
-  for (const version in changelogData) {
-    parsedData.push(`#### ${version}`);
-    Reader(changelogData[version]);
-    parsedData.push("---");
-  }
-  parsedData.pop();
+interface ChangelogPage {
+  version: string;
+  lines: string[];
+}
 
-  // eslint-disable-next-line react/no-children-prop
-  return <ReactMarkdown children={parsedData.join("\n")} />;
+const Changelog: React.FC = () => {
+  const [curPage, setCurPage] = useState(0);
+
+  const changelogPages = (() => {
+    const retval: ChangelogPage[] = [];
+    let curCollection: string[] = [];
+    let curFirstLine: string | null = null;
+    let curLastLine: string | null = null;
+    let verCount = 0;
+
+    for (const [version, lines] of Object.entries(changelogData)) {
+      if (++verCount % 10 === 0) {
+        curCollection.pop();
+        retval.push({ version: `${curFirstLine}...${curLastLine}`, lines: curCollection });
+        curCollection = [];
+      }
+
+      /**
+       * Reads a list of changelog data to be displayed.
+       * @param data The data to read.
+       * @param level The nesting level of the current list.
+       */
+      const Reader = (data: ChangelogLine, level = 0) => {
+        const spacing = (() => {
+          let retval = "";
+          for (let x = 0; x < level; x++) retval += "  ";
+          return retval + "- ";
+        })();
+        for (const line of data)
+          if (Array.isArray(line)) Reader(line, level + 1);
+          else curCollection.push(spacing + line);
+      };
+
+      if (curFirstLine === null) curFirstLine = version;
+      curLastLine = version;
+
+      curCollection.push(`#### ${version}`);
+      Reader(lines);
+      curCollection.push("---");
+    }
+
+    curCollection.pop();
+    retval.push({ version: `${curFirstLine}...${curLastLine}`, lines: curCollection });
+
+    return retval;
+  })();
+
+  const pageData = changelogPages[curPage];
+
+  return (<>
+    <button onClick={() => setCurPage(Math.max(curPage - 1, 0))} disabled={curPage === 0}>Prev</button>
+    <button onClick={() => setCurPage(Math.min(curPage + 1, changelogPages.length - 1))} disabled={curPage + 1 == changelogPages.length}>Next</button><br />
+    {/* eslint-disable-next-line react/no-children-prop */}
+    <ReactMarkdown children={pageData.lines.join("\n")} />
+  </>);
 };
 
 export default Changelog;
