808413196b6f96801dcd92750a7d934fab7463f8
Felix R <felicitusneko@duck.com>
2022-09-06 02:33:02 -0300

    actually I can apparently just do this?

--- src/index.tsx
+++ src/index.tsx
@@ -5,13 +5,11 @@ import * as serviceWorker from "./serviceWorker";
 import "./index.css";
 import SettingsTool from "./routes/SettingsTool";
 
-const root = document.getElementById("root")
-if (root) createRoot(root).render(
+createRoot(document.body).render(
   <React.StrictMode>
     <SettingsTool />
   </React.StrictMode>
 );
-else throw new Error("Document root not found");
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
